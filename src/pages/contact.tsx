ce6d42cbcc65711cc3755b0c5f20762a4bf1925f
Bailey McKelway <bjlmckelway@gmail.com>
2021-01-26 21:50:57 -0500

    feat(footer): added links to personal social media on contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Contact: React.FC = () => (
   <div className="container">
@@ -9,6 +11,23 @@ const Contact: React.FC = () => (
       If there is any support needed or you just want to reach out please email:
       <a href="mailto:contact@mediaedits.io"> contact@mediaedits.io </a>
     </p>
+    <p>Contact to creator!</p>
+    <div>
+      <a
+        href="https://www.linkedin.com/in/bailey-mckelway/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedInIcon fontSize="large" className="social_links" />
+      </a>
+      <a
+        href="https://github.com/BaileyMcKelway"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <GitHubIcon fontSize="large" className="social_links" />
+      </a>
+    </div>
     <Footer />
   </div>
 );
